24864a928edbb2fbd0afb879b535b4511da8a673
George <developer@monternos.net>
2019-12-06 21:00:13 +0100

    [added] home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<div class="home">
+		<img class="party__gif" src="@/assets/party.gif" alt="Party Gif" />
+		<a class="party__link" target="_blank" href="https://github.com/Keimeno">
+			<img src="@/assets/GitHub-Mark-Light-32px.png" alt="Github" />
+		</a>
+	</div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
-};
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+	name: "home"
+});
 </script>
+
+<style lang="scss" scoped>
+.home {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+
+	.party__gif {
+		margin-bottom: 40px;
+		background-color: rgb(26, 26, 26);
+		border-radius: 5px;
+		padding: 10px;
+		box-shadow: 0px 3px rgba($color: rgb(26, 26, 26), $alpha: 0.7);
+	}
+}
+</style>
